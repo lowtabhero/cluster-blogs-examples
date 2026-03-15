b5f2b28ea1eca892ddbc2365480ab74b364c99ca
Alexey <lowtabhero@mail.ru>
2026-03-15 21:04:00 +0100

    add card image placeholder on load fail

--- src/lib/components/BlogCard.svelte
+++ src/lib/components/BlogCard.svelte
@@ -3,11 +3,17 @@
 	import type { NormalizedBlog } from '$lib/NormalizedBlogType.js';
 
 	let { data, lang }: { data: NormalizedBlog; lang: string } = $props();
+
+	let cardImage: HTMLImageElement;
+
+	function onLoadError() {
+		cardImage.src = 'https://wordpress.stockscout.eu/wp-content/uploads/2026/02/1.png';
+	}
 </script>
 
 <a href="/{lang}/{data.slug}" class="blog-card">
 	<div class="featured-image">
-		<img src={data.featuredMedia} alt="" />
+		<img src={data.featuredMedia} alt="" onerror={onLoadError} bind:this={cardImage} />
 	</div>
 	<div class="info">
 		<div class="up">
